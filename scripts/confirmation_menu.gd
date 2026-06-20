3626d9df46d02f23de618c704c4bf897da253497
JackieTheWackie <lowery.jackie20@gmail.com>
2026-06-20 16:04:18 -0400

    Adding drink menu that links from confirmation menu

--- scripts/confirmation_menu.gd
+++ scripts/confirmation_menu.gd
@@ -1,11 +1,14 @@
 extends Control
 
-@onready var yes_button: Button = $PanelContainer/VBoxContainer/Button
+@onready var yes_button: Button = $PanelContainer/VBoxContainer/Yes
+@onready var no_button: Button = $PanelContainer/VBoxContainer/No
+@onready var drink_menu: Control = $"../../Drink Menu/Control"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
 	yes_button.pressed.connect(_on_yes_pressed)
+	no_button.pressed.connect(_on_no_pressed)
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -23,4 +26,8 @@ func close_menu() -> void:
 	hide()
 
 func _on_yes_pressed() -> void:
+	hide()
+	drink_menu.show()
+
+func _on_no_pressed() -> void:
 	close_menu()
