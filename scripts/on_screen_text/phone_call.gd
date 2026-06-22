e01a42ab952679a8de2e7b598a390bef089a132c
JackieTheWackie <lowery.jackie20@gmail.com>
2026-06-21 21:39:14 -0400

    Adding timer and organizing scripts

--- scripts/on_screen_text/phone_call.gd
+++ scripts/on_screen_text/phone_call.gd
@@ -1,9 +1,9 @@
-extends Node
+extends TextEdit
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
