8770752a42d432ec964545e6617d0672123627c9
JackieTheWackie <lowery.jackie20@gmail.com>
2026-06-24 19:23:42 -0400

    Adding actual hints that line up with the correct drink

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,8 +7,73 @@ var target_correct: bool = false
 var selected_drink: int = 0
 var selected_target: int = 0
 var decision_time: bool = false
-var drink_list = [$"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/1", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/2", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/3", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/4",
-				  $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/5", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/6", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/7", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/8"]
+
+@onready var drink_hints = [
+	{
+		"number": 1,
+		"hints": [
+			"Glass with a stem",
+			"Flavored Rim",
+			"Decorative Garnish"
+		]
+	},
+	{
+		"number": 2,
+		"hints": [
+			"Glass with a stem",
+			"Swirly Liquid",
+			"Third thing"
+		]
+	},
+	{
+		"number": 3,
+		"hints": [
+			"Glass without a stem",
+			"On the rocks",
+			"Garnish"
+		]
+	},
+	{
+		"number": 4,
+		"hints": [
+			"Glass without a stem",
+			"On the rocks",
+			"Fizzy liquid"
+		]
+	},
+	{
+		"number": 5,
+		"hints": [
+			"Glass with a stem",
+			"Swirly liquid",
+			"Fruit Garnish"
+		]
+	},
+	{
+		"number": 6,
+		"hints": [
+			"Glass with a stem",
+			"Flavored Rim",
+			"Submerged Fruit"
+		]
+	},
+	{
+		"number": 7,
+		"hints": [
+			"Glass without a stem",
+			"Fizzy Liquid",
+			"Fruit Garnish"
+		]
+	},
+	{
+		"number": 8,
+		"hints": [
+			"Glass with a stem",
+			"Decorative Garnish",
+			"Submerged Fruit"
+		]
+	}
+]
 
 func _ready() -> void:
 	actual_drink = randi_range(1,8)
--- scripts/on_screen_text/timer.gd
+++ scripts/on_screen_text/timer.gd
@@ -15,12 +15,20 @@ extends Node
 @onready var hint_range = 15
 
 # Buffer time between hints
-@onready var hint_buffer = 10;
+@onready var hint_buffer = 10
+
+@onready var drink_hints = []
+@onready var current_drink_hint = 0
+@onready var has_incremented = false
 
 func _ready() -> void:
 	hint_timing = rng.randi_range(timer.wait_time - 15, timer.wait_time - 3)
 	timer.timeout.connect(_on_timeout)
-	
+	for hint in GameManager.drink_hints:
+		if (hint.get("number") == GameManager.actual_drink):
+			drink_hints = hint.get("hints")
+			break
+	drink_hints.shuffle()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,18 +37,24 @@ func _process(delta: float) -> void:
 	else:
 		timer.stop()
 		label.hide()
-	
+		phone_call.hide()
+		waiter.hide()
+
 
 func _time_left():
 	var time_left = timer.time_left
 	
 	# If it's time for a hint
 	if hint_timing == floori(time_left):
-		# Show the hint (alternating between phone and waiter)
-		if phone_hint:
-			phone_call.show()
-		else:
-			waiter.show()
+		if not has_incremented:
+			# Show the hint (alternating between phone and waiter)
+			if phone_hint:
+				phone_call.text = drink_hints[current_drink_hint]
+				current_drink_hint += 1
+				has_incremented = true
+				phone_call.show()
+			else:
+				waiter.show()
 	# After a certain amount of time, hide currently showing hint
 	elif hint_timing - hint_length == floori(time_left):
 		if phone_hint:
@@ -56,10 +70,13 @@ func _time_left():
 			hint_timing = 20
 		else:
 			hint_timing = rng.randi_range(timer.time_left - hint_range, timer.time_left - hint_buffer)
-		
+	else:
+		has_incremented = false
+	
 	var minute = floor(time_left / 60)
 	var second = int(time_left) % 60
 	return [minute, second]
+
 
 func _on_timeout() -> void:
 	drink_menu.show()
