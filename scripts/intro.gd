719a068ba7697de3bedbc59236b5be629b621173
Juliana <49417934+JulianaLSch@users.noreply.github.com>
2026-06-25 15:00:24 -0400

    Intro messages

---
+++ scripts/intro.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var intro_1: Control = $Intro1
+@onready var intro_2: Control = $Intro2
+@onready var intro_3: Control = $Intro3
+@onready var intro_4: Control = $Intro4
+@onready var intro_5: Control = $Intro5
+
+@onready var start: Button = $Start
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	intro_1.visible = true
+	intro_2.visible = false
+	intro_3.visible = false
+	intro_4.visible = false
+	intro_5.visible = false
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		intro_1.visible = false
+		intro_2.visible = true
+		intro_3.visible = false
+		intro_4.visible = false
+		intro_5.visible = false
