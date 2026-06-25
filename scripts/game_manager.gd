0818936c30645522ea331278e51f69b48f57b400
JackieTheWackie <lowery.jackie20@gmail.com>
2026-06-24 21:48:21 -0400

    Revert "Adding result screen and stopping timer when decision making starts"

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,57 +6,8 @@ var drink_correct: bool = false
 var target_correct: bool = false
 var selected_drink: int = 0
 var selected_target: int = 0
-var decision_time: bool = false
-
-@onready var drink_hints = [
-	{
-		"number": 1,
-		"hints": ["Glass with a stem", "Flavored Rim", "Decorative Garnish"]
-	},
-	{
-		"number": 2,
-		"hints": ["Glass with a stem", "Swirly Liquid", "Third Thing"]
-	},
-	{
-		"number": 3,
-		"hints": ["Glass without a stem", "On the rocks", "Garnish"]
-	},
-	{
-		"number": 4,
-		"hints": ["Glass without a stem", "On the rocks", "Fizzy Liquid"]
-	},
-	{
-		"number": 5,
-		"hints": ["Glass with a stem", "Swirly Liquid", "Fruit Garnish"]
-	},
-	{
-		"number": 6,
-		"hints": ["Glass with a stem", "Flavored Rim", "Submerged Fruit"]
-	},
-	{
-		"number": 7,
-		"hints": ["Glass without a stem", "Fizzy Liquid", "Fruit Garnish"]
-	},
-	{
-		"number": 8,
-		"hints": ["Glass with a stem", "Decorative Garnish", "Submerged Fruit"]
-	}
-]
-
-@onready var target_hints = [
-	{
-		"number": 1,
-		"hints": ["Target 1", "Target 2", "Target 3"]
-	},
-	{
-		"number": 2,
-		"hints": ["Target 1", "Target 2", "Target 3"]
-	},
-	{
-		"number": 3,
-		"hints": ["Target 1", "Target 2", "Target 3"]
-	}
-]
+var drink_list = [$"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/1", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/2", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/3", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer/4",
+				  $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/5", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/6", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/7", $"Drink Menu/Control/PanelContainer/VBoxContainer/HBoxContainer2/8"]
 
 func _ready() -> void:
 	actual_drink = randi_range(1,8)
